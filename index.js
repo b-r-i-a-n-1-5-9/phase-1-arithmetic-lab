b7dcbebb5322c4819a7cccf0d913eaa991cd5338
Brian Mwiti <briangarang@gmail.com>
2024-09-30 17:52:17 +0300

    created three functions, multiply, random and max, making sure they alighn with test guidelines

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+//multiply
 
-// Write your code here
+let num1=31;
+let num2=2;
+let multiply= num1*num2;
+// a random integer greater than 0
+let random= Math.ceil(Math.random());
+// mod is an equation that calculates a remaindeer that is equal to 4
+
+let num3= 10;
+let num4= 6;
+let mod = num3 % num4;
+
+let numbers=(12,15,16,17,18,19,20);
+let max = Math.max(numbers)
